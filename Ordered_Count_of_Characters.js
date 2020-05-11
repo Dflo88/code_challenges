424a01fd6439993131977c82b7e45c81b96ef028
Diego <dflores7237@gmail.com>
2020-05-11 11:31:48 -0700

    added instructions

--- Ordered_Count_of_Characters.js
+++ Ordered_Count_of_Characters.js
@@ -1,3 +1,12 @@
+/* 
+Instructions: 
+Count the number of occurrences of each character and return it 
+as a list of tuples in order of appearance. For empty output return 
+an empty list.
+
+source: codewars 7kyu
+*/
+
 var orderedCount = function (text) {
     var tally = {};
     let mySet = new Set(text);
@@ -5,6 +14,7 @@ var orderedCount = function (text) {
     for (x of text) {
       tally[x] ? tally[x] = tally[x] + 1: tally[x] = 1;
     }
+
     mySet.forEach((element) => finalArray.push([element, tally[element]])); 
     return(finalArray)
 }
